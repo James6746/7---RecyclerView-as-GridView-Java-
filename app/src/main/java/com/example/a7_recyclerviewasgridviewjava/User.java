aa839d81dab4adeec06bd745c88da68171ea543f
James <sobirovjamshid18@gmail.com>
2022-01-15 14:51:54 +0500

    first commit

---
+++ app/src/main/java/com/example/a7_recyclerviewasgridviewjava/User.java
@@ -0,0 +1,13 @@
+package com.example.a7_recyclerviewasgridviewjava;
+
+public class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
